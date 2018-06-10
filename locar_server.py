510b4447fab2f22483fd8ba979e52c001301ab7e
puto el que lo lea <saramirez@unicauca.edu.co>
2018-06-10 10:33:29 -0500

    Estoy trabajando con javascript, ejecuto una funcion cada que se checkea un checkbox

--- locar_server.py
+++ locar_server.py
@@ -1,15 +1,17 @@
 import SimpleHTTPServer
 import SocketServer
-
 PORT = 8001
 class MyHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	def do_GET(self):
 		if self.path == '/':
-			self.path = '/design.html'
+			self.path = '/javascript.html'
 			return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
+
+
 Handler = MyHandler
-
 httpd = SocketServer.TCPServer(("", PORT), Handler)
-
 print "serving at port", PORT
 httpd.serve_forever()
+
